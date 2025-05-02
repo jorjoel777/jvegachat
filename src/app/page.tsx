c9477712ee2e6cc43611ce2adeee4cdc84c59a98
Jorge Vega <jorbre@gmail.com>
2025-05-02 16:00:36 +0200

    fixing logo for vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center max-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-[22px] row-start-2 items-end sm:flex-row sm:justify-between sm:w-full">
          <Image
-           src="/jorgevegafullstack_logo.svg"
+           src="/jvfullstack_logo.png"
            alt="Jorge Vega FullStack Services logo"
            width={180}
            height={38}
